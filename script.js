d1aecbc1041a1b150722016b076af92c8c44d1a0
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-19 10:27:32 +0200

    array intro

--- script.js
+++ script.js
@@ -10,10 +10,60 @@ let userPatryk = {
     birthDate: 1980
 }
 
-// to access data inside the object:
-console.log(userPatryk) // prints entire object, not that useful, usually we want one item at a time, f.eks:
+let userBob = {
+    name: "Bob",
+    location: "Oslo",
+    birthDate: 2002
+}
 
-console.log(userPatryk.location)
-console.log(userPatryk.name)
+
+let userAnna = {
+    name: "Anna",
+    location: "Tromsø",
+    birthDate: 1999
+}
+
+function greet(userInfo) {
+    // vi printer nå ut navn og sted
+    // jeg ønsker også å printe ut alder 
+    let userAge = 2026 - userInfo.birthDate
+
+    console.log("Hi my name is " + userInfo.name + ", i am from " + userInfo.location + ". And i am " + userAge  + " years old")
+}
 
 
+greet(userPatryk)
+greet(userBob)
+greet(userAnna)
+
+// for å oppsumere object:
+// en object er en samling av "key, value" - pairs
+
+
+// 2. Arrays (på norsk liste)
+
+// a list of numbers
+let number1 = 1;
+let number2 = 2;
+let number3 = 3;
+let number4 = 4; // osv...
+
+// vi kan også lagre en liste type (array), på denne måten:
+let myNumbers = [51, 22, 5819815,9,6,7]
+
+console.log(myNumbers)
+
+let user1 = "Patryk"
+let user2 = "Hans"
+let user3 = "Maria"
+let user4 = "Hanna"
+
+// samme men i et array...
+
+let users = ["Patryk", "Hans", "Maria", "Hanna"]
+
+console.log(users)
+
+// an array does not have named keys, instead they have numbered indexes:
+
+console.log(users[0]) // gets the first value, arrays always start at 0
