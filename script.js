1eff86ab0abb62924275874897a114d246ee6c4d
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-19 10:41:53 +0200

    select and change elements from javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+// for å hente elementer i javascript...
+
+let myElement = document.querySelector("h1") // samme som i css `h1 { kode }`
+
+console.log(myElement)
+
+// for å endre på innholdet, eller stylen til elemented må vi bruke de innebygde verktøyene...
+
+// 1. Endre tekst
+// myElement.textContent = "test"
+
+// 2. endre style (css)
+// myElement.style = "color: red;" // css koden må da være inni "" (quotation marks)
+
