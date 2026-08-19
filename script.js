1b39c1d0894b7675e1df3eb9c7050253655ff950
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-19 09:30:43 +0200

    basic types examples (string, numbers)

---
+++ script.js
@@ -0,0 +1,21 @@
+// skrive en melding til console
+
+// skrive ut text
+console.log("god morgen")
+console.log("melding")
+
+console.log("hei " + " hvordan går det?")
+
+console.log("2" + "2")
+// skrive ut tall
+
+console.log(2 + 2)
+
+console.log(5 - 3) // også console.log("5" - "3") virker :)
+
+console.log(5 * 5)
+
+console.log(3 / 2)
+
+
+
